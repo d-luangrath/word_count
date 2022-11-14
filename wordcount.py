c5c9a1d04357b9f85d63f5cbe49c07175833795b
Darla Luangrath <darla.luangrath@gmail.com>
2022-11-14 14:41:06 -0800

    Implement tokenize fucntion

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,9 @@
 """Count words in file."""
+import sys
 
-f = open("test.txt")
-number_of_word_occurences = {}
+filename = sys.argv[1]
+# f = open(filename)
+# number_of_word_occurences = {}
 
 #for loop to iterate thru each line to get word
 #remove white space at end of line, get words into a list
@@ -12,12 +14,26 @@ number_of_word_occurences = {}
 
 #when done, for loop to iterate thru items in the list and print the "word" and its "count"
 
-for line in f:
-    line = line.rstrip().split()
+# for line in f:
+#     line = line.rstrip().split()
     
-    for word in line:
-        number_of_word_occurences[word] = number_of_word_occurences.get(word, 0) + 1
+#     for word in line:
+#         number_of_word_occurences[word] = number_of_word_occurences.get(word, 0) + 1
 
-for word, count in number_of_word_occurences.items():
-    print(word, count)
+# for word, count in number_of_word_occurences.items():
+#     print(word, count)
 
+
+def tokenize():
+    '''Returns list of words from a file'''
+    token = []
+
+    with open(filename) as f:
+        for line in f:
+            stripped_lines = line.rstrip().split()
+            print(type(stripped_lines))
+            token.extend(stripped_lines)
+
+    return token
+
+print(tokenize())
